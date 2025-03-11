2101960612fc5448c99a51ecb581a9af2da74e28
Sweetreams <pashasurov12345@gmail.com>
2025-03-11 15:09:15 +0700

    return

--- src/main.jsx
+++ src/main.jsx
@@ -4,62 +4,61 @@ import './index.css'
 import AuthUser from './components/page/authUser/AuthUser.jsx'
 import RegUser from './components/page/regUser/RegUser.jsx'
 
-// import DataAnalysis from './components/page/dataAnalysisPage/DataAnalysis.jsx'
-// import AboutProjectPage from './components/page/aboutProjectPage/AboutProjectPage.jsx'
-// import DocumentationPage from './components/page/documentationPage/DocumentationPage.jsx'
-// import ProfilePage from './components/page/profilePage/ProfilePage.jsx'
-// import PublicDataPage from './components/page/publicDataPage/PublicDataPage.jsx'
-// import PublicDataSearch from './components/page/publicDataSearch/PublicDataSearch.jsx'
+import DataAnalysis from './components/page/dataAnalysisPage/DataAnalysis.jsx'
+import AboutProjectPage from './components/page/aboutProjectPage/AboutProjectPage.jsx'
+import DocumentationPage from './components/page/documentationPage/DocumentationPage.jsx'
+import ProfilePage from './components/page/profilePage/ProfilePage.jsx'
+import PublicDataPage from './components/page/publicDataPage/PublicDataPage.jsx'
+import PublicDataSearch from './components/page/publicDataSearch/PublicDataSearch.jsx'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import '@ant-design/v5-patch-for-react-19';
 
 const router = createBrowserRouter([
-  // {
-  //   path: '/',
-  //   element: <App/>,
-  //   children:[
-  //     {
-  //       path: 'dataanalysis',
-  //       element: <DataAnalysis/>
-  //     },
-  //     {
-  //       path: 'aboutproject',
-  //       element: <AboutProjectPage/>
-  //     },
-  //     {
-  //       path: 'documentation',
-  //       element: <DocumentationPage/>
-  //     },
-  //     {
-  //       path: 'profile',
-  //       element: <ProfilePage/>
-  //     },
-  //     {
-  //       path: 'publicdate',
-  //       element: <PublicDataPage/>
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {
+        path: 'dataanalysis',
+        element: <DataAnalysis/>
+      },
+      {
+        path: 'aboutproject',
+        element: <AboutProjectPage/>
+      },
+      {
+        path: 'documentation',
+        element: <DocumentationPage/>
+      },
+      {
+        path: 'profile',
+        element: <ProfilePage/>
+      },
+      {
+        path: 'publicdate',
+        element: <PublicDataPage/>
         
-  //     },
-  //     {
-  //       path: 'publicdate/:id',
-  //       element: <PublicDataSearch/>
-  //     }
-  //   ]
-  // },
-  // {
-  //   path:'/auth',
-  //   element: <AuthUser/>
-  // },
-  // {
-  //   path:'/reg',
-  //   element: <RegUser/>
-  // }
+      },
+      {
+        path: 'publicdate/:id',
+        element: <PublicDataSearch/>
+      }
+    ]
+  },
+  {
+    path:'/auth',
+    element: <AuthUser/>
+  },
+  {
+    path:'/reg',
+    element: <RegUser/>
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthUser/>
-    {/* <RouterProvider router={router}/> */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
