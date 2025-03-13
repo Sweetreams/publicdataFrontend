fb419efc7d0c5b8fe7a7252f31dc9477b13de2d6
Sweetreams <pashasurov12345@gmail.com>
2025-03-13 21:06:46 +0700

    asfasf

--- src/main.jsx
+++ src/main.jsx
@@ -59,6 +59,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <div>sdafsdf</div>
+    {/* <RouterProvider router={router}/> */}
   </StrictMode>,
 )
