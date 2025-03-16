7727e96e30db0859032b639cd412646823ba5007
Sweetreams <pashasurov12345@gmail.com>
2025-03-16 13:04:10 +0700

    public version

--- src/components/page/aboutProjectPage/AboutProjectPage.jsx
+++ src/components/page/aboutProjectPage/AboutProjectPage.jsx
@@ -10,18 +10,18 @@ const AboutProjectPage = () => {
 
     
       <div style={{maxWidth: '900px'}}>
-        <Typography.Title level={4} style={{ paddingBottom: '13.2px' }}>О проекте</Typography.Title>
+        <Typography.Title level={3} style={{ paddingBottom: '13.2px' }}>О проекте</Typography.Title>
         <div style={{ textAlign: 'left' }}>
           <Typography.Text>Этот сайт — мой pet-проект, созданный с целью исследования, анализа и визуализации публичных данных. Здесь я собираю, обрабатываю и представляю информацию, которая может быть полезна для всех, кто интересуется аналитикой, статистикой и открытыми данными.</Typography.Text>
-          <Typography.Title level={5}>Зачем этот проект?</Typography.Title>
+          <Typography.Title level={4}>Зачем этот проект?</Typography.Title>
           <Typography.Text>Я всегда увлекался работой с данными и хотел создать платформу, где можно было бы не только изучать, но и делиться результатами своих исследований. Этот сайт — моя попытка сделать аналитику публичных данных более доступной и понятной для широкой аудитории.</Typography.Text>
-          <Typography.Title level={5}>Что вы найдете здесь?</Typography.Title>
+          <Typography.Title level={4}>Что вы найдете здесь?</Typography.Title>
           <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
             <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Аналитика публичных данных</b>: Я собираю данные из открытых источников (государственные порталы, социальные сети, публичные API и т.д.), анализирую их и делюсь своими выводами.</Typography.Text></li>
             <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Визуализация</b>: Сложные данные становятся понятнее, когда они представлены в виде графиков, диаграмм и интерактивных карт. Здесь вы найдете наглядные примеры.</Typography.Text></li>
             <li><Typography.Text><b>Инструменты для анализа</b>: Я разрабатываю и тестирую различные методы обработки данных, которыми можно воспользоваться для своих исследований.</Typography.Text></li>
           </ul>
-          <Typography.Title level={5}>Технологии</Typography.Title>
+          <Typography.Title level={4}>Технологии</Typography.Title>
           <Typography.Text >Для реализации проекта используются современные инструменты и технологии:</Typography.Text>
 
           <ul type="disc" style={{ paddingBottom: '19.2px', paddingTop: '10px', paddingLeft: '24px' }}>
@@ -29,9 +29,9 @@ const AboutProjectPage = () => {
             <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Библиотеки для анализа и визуализации</b>: antd, antd-charts</Typography.Text></li>
             <li><Typography.Text><b>Библиотеки для backend'a</b>: express-js, prisma.</Typography.Text></li>
           </ul>
-          <Typography.Title level={5}>Связь</Typography.Title>
+          <Typography.Title level={4}>Связь</Typography.Title>
           <Typography.Text>Если у вас есть вопросы, предложения или вы хотите поделиться идеями, напишите мне на pashasurov12345@gmail.com или свяжитесь через telegram: @fireAlarmDoubleRR. Буду рад обратной связи!</Typography.Text>
-          <Typography.Title level={5} style={{ paddingBottom: '19.2px' }}>Планы на будущее</Typography.Title>
+          <Typography.Title level={4} style={{ paddingBottom: '19.2px' }}>Планы на будущее</Typography.Title>
         </div>
         <div className="container">
           <div className="left_container">
@@ -43,9 +43,9 @@ const AboutProjectPage = () => {
                   title: 'Расширение функционала',
                   description:
                     <>
-                      <div >1. Интерактивные инструменты: </div>
-                      <ul style={{ paddingLeft: '20px', paddingBottom: '10px' }}>
-                        <li style={{ paddingBottom: '7px', paddingTop: '7px' }}>Добавить возможность фильтрации данных на сайте (например, по регионам, временным периодам)</li>
+                      <div style={{marginTop: '7px'}}>1. Интерактивные инструменты: </div>
+                      <ul style={{ paddingLeft: '20px', paddingBottom: '15px', }}>
+                        <li style={{ paddingBottom: '10px', paddingTop: '10px' }}>Добавить возможность фильтрации данных на сайте (например, по регионам, временным периодам)</li>
                         <li>Создать простые калькуляторы или аналитические инструменты (например, прогнозирование на основе данных).</li>
                       </ul>
                       <div >2. Улучшение визуализации: </div>
@@ -58,9 +58,9 @@ const AboutProjectPage = () => {
                 {
                   title: 'Масштабирование и автоматизация',
                   description: <>
-                    <div >1. Автоматизация сбора данных: </div>
-                    <ul style={{ paddingLeft: '20px', paddingBottom: '10px' }}>
-                      <li style={{ paddingBottom: '7px', paddingTop: '7px' }}>Настроить регулярный парсинг и обновление данных из источников.</li>
+                    <div style={{marginTop: '7px'}}>1. Автоматизация сбора данных: </div>
+                    <ul style={{ paddingLeft: '20px', paddingBottom: '15px', }}>
+                      <li style={{ paddingBottom: '10px', paddingTop: '10px' }}>Настроить регулярный парсинг и обновление данных из источников.</li>
                       <li>Реализовать уведомления о новых данных или изменениях в существующих.</li>
                     </ul>
                     <div >2. Расширение тематики: </div>
@@ -74,14 +74,14 @@ const AboutProjectPage = () => {
                 {
                   title: 'Долгосрочные цели',
                   description: <>
-                    <div >1. Мобильная версия и приложение: </div>
-                    <ul style={{ paddingLeft: '20px', paddingBottom: '10px' }}>
-                      <li style={{ paddingBottom: '7px', paddingTop: '7px' }}>Разработать мобильную версию сайта или приложение для удобного доступа к данным.</li>
+                    <div style={{marginTop: '7px'}}>1. Мобильная версия и приложение: </div>
+                    <ul style={{ paddingLeft: '20px', paddingBottom: '15px', }}>
+                      <li style={{ paddingBottom: '10px', paddingTop: '10px' }}>Разработать мобильную версию сайта или приложение для удобного доступа к данным.</li>
                       <li>Добавить push-уведомления о новых отчетах или обновлениях.</li>
                     </ul>
                     <div >2. API для разработчиков: </div>
-                    <ul style={{ paddingLeft: '20px', paddingBottom: '10px' }}>
-                      <li style={{ paddingBottom: '7px', paddingTop: '7px' }}>Создать открытое API для доступа к данным сайта.</li>
+                    <ul style={{ paddingLeft: '20px', paddingBottom: '15px', }}>
+                      <li style={{ paddingBottom: '10px', paddingTop: '10px' }}>Создать открытое API для доступа к данным сайта.</li>
                       <li>Разработать документацию для интеграции с другими проектами.</li>
                     </ul>
                     <div >3. Машинное обучение и AI: </div>
--- src/components/page/documentationPage/DocumentationPage.jsx
+++ src/components/page/documentationPage/DocumentationPage.jsx
@@ -1,10 +1,80 @@
-import { Typography } from 'antd'
+import { Space, Typography } from 'antd'
 import React from 'react'
 
 const DocumentationPage = () => {
   document.title = 'Документация'
   return (
-    <Typography.Title level={4}>Документация</Typography.Title>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+
+
+      <div style={{ maxWidth: '900px' }}>
+        <Typography.Title level={3} style={{ paddingBottom: '13.2px' }}>Документация</Typography.Title>
+        <div style={{ textAlign: 'left' }}>
+          <Typography.Title level={4}>Как пользоваться сайтом</Typography.Title>
+          <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Навигация:</b><br />На главной странице вы найдете ссылки на основные разделы: "Данные", "Аналитика", "Документация" и "О проекте". Используйте меню в левой части сайта для быстрого перехода.</Typography.Text></li>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Публичные данные:</b><br />В разделе "Данные" содержатся публичные данные, которые можно: просомтреть, отфильтровать и отсортировать.</Typography.Text></li>
+            <li><Typography.Text><b>Аналитика:</b><br />В разделе "Аналитика" содержатся инструменты для анализа и визуализации публичных данных.</Typography.Text></li>
+          </ul>
+
+          <Typography.Title level={4}>Работа с данными</Typography.Title>
+          <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Источники данных:</b><br />Мы используем только открытые данные из публичных источников: государственные порталы, API, социальные сети и другие ресурсы. Список источников доступен в каждом отчете.</Typography.Text></li>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Форматы данных:</b><br />Данные можно скачать в форматах CSV, JSON и Excel для дальнейшего анализа.</Typography.Text></li>
+            <li><Typography.Text><b>Обновление данных:</b><br />Данные обновляются автоматически. Даты последнего обновления указаны в каждом разделе.</Typography.Text></li>
+          </ul>
+
+          <Typography.Title level={4}>Инструменты для анализа</Typography.Title>
+          <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Визуализация:</b><br />Используйте встроенные графики, диаграммы и карты для анализа данных. Вы можете настраивать отображение, выбирая параметры и фильтры.</Typography.Text></li>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Сортировка:</b><br />Все числовые данные можно отсортировать.</Typography.Text></li>
+            <li><Typography.Text><b>Фильтрация:</b><br />Все текстовые данные можно отфильтровать</Typography.Text></li>
+          </ul>
+
+          <Typography.Title level={4}>Инструменты для анализа</Typography.Title>
+          <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
+            <li style={{ paddingBottom: '7px' }}>
+              <Space direction='vertical' size="middle">
+                <Typography.Text>
+                  <b>Пример 1: Анализ ВВП стран мира: </b>
+                  <br />
+                  Использование графиков, для анализа и визуализации данных.
+                </Typography.Text>
+                <img src="/example1.png" alt="example1" style={{ width: "500px" }} />
+              </Space>
+            </li>
+            <li style={{ paddingBottom: '7px' }}>
+              <Space direction='vertical' size="middle">
+                <Typography.Text>
+                  <b>Пример 2: Анализ ВВП стран мира и запасы железных руд: </b>
+                  <br />
+                  Использование графиков, для анализа и визуализации различных данных.
+                </Typography.Text>
+                <img src="/example2.png"  style={{ width: "500px" }} />
+              </Space>
+            </li>
+            <li>
+              <Space direction='vertical' alt="example2" size="middle">
+                <Typography.Text>
+                  <b>Пример 3: Анализ универитетов стран мира и продажи новых автомобилей: </b>
+                  <br />
+                  Использование различных графиков, для анализа и визуализации данных
+                </Typography.Text>
+                <img src="/example3.png" alt="example3" style={{ width: "500px" }} />
+              </Space>
+            </li>
+          </ul>
+
+          <Typography.Title level={4}>Часто задаваемые вопросы (FAQ)</Typography.Title>
+          <ul type="disc" style={{ paddingBottom: '19.2px', paddingLeft: '24px' }}>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Как часто обновляются данные?</b><br />Данные обновляются раз в пол года</Typography.Text></li>
+            <li style={{ paddingBottom: '7px' }}><Typography.Text><b>Можно ли использовать данные в коммерческих целях?</b><br />Да, все данные открыты и доступны для использования. Убедитесь, что вы соблюдаете лицензии источников.</Typography.Text></li>
+            <li><Typography.Text><b>Как связаться с поддержкой?</b><br />Напишите мне на почту: pashasurov12345@gmail.com</Typography.Text></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
